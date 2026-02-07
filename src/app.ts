4319e72978112a5e57b9570f1baa70af8a854dc0
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-02-07 18:09:11 +0600

    Email & Password Authentication

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 
 
@@ -11,6 +14,6 @@ app.use("/posts", postRouter);
 
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 export default app;

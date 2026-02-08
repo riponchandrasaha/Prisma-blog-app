2fba2332c520b51688d439a12498471d0197ab6f
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-02-08 13:52:21 +0600

    Authentication Middleware

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,8 +1,22 @@
-import express, { Router } from "express";
+import express, { NextFunction, Request, Response, Router } from "express";
 import { PostController } from "./post.controller";
-
+import { auth as betterAuth } from "../../lib/auth";
 const router = express.Router();
-router.post("/",
+const auth = (...roles: any) => {
+    // Simulated async auth function
+    return async (req: Request, res: Response, next: NextFunction) => {
+
+        const session = await betterAuth.api.getSession({
+            headers: req.headers as any
+        })
+        console.log(session);
+
+
+    }
+}
+router.post(
+    "/",
+    auth("ADMIN"),
     PostController.createPost
- );
-export const postRouter:Router = router;
+);
+export const postRouter: Router = router;

a6cdc853f0c4fb628e4adf7dc87762c3561aa7d2
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-02-08 15:43:29 +0600

    Production-Ready Security

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,67 @@
+import { auth as betterAuth } from "../lib/auth";
+import { NextFunction, Request, Response } from "express";
+
+export enum UserRole {
+    USER = "USER",
+    ADMIN = "ADMIN"
+}
+
+/* ===== Express Request Type Augmentation ===== */
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        name: string;
+        role: UserRole;
+        emailVerified: boolean;
+      };
+    }
+  }
+}
+
+
+/* ===== Auth Middleware ===== */
+const auth = (...roles: UserRole[]) =>
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const session = await betterAuth.api.getSession({
+                headers: req.headers as any,
+            });
+
+            if (!session) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Unauthorized",
+                });
+            }
+
+            if (!session.user.emailVerified) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Email verification required",
+                });
+            }
+
+            req.user = {
+                id: session.user.id,
+                email: session.user.email,
+                name: session.user.name,
+                role: session.user.role as UserRole,
+                emailVerified: session.user.emailVerified,
+            };
+
+            if (roles.length && !roles.includes(req.user.role)) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Forbidden: You don't have permission",
+                });
+            }
+
+            next(); // ✅ VERY IMPORTANT
+        } catch (err) {
+            next(err);
+        }
+    };
+    export default auth;
--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -6,8 +6,16 @@ import { Post } from "../../../generated/prisma/client";
 const createPost = async (req: Request, res: Response) => {
 
   try {
-    console.log(req.user)
-    const result = await postService.createPost(req.body);
+    const user = req.user;
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Unauthorized access"
+      })
+
+    }
+    /*  console.log(req.user) */
+    const result = await postService.createPost(req.body, user.id as string);
     res.status(201).json(result)
   } catch (e) {
     res.status(400).json({

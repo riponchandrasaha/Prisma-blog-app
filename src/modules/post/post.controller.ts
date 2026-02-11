b5517f010c55a86dc65440c9765da377a22b5a10
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-02-11 20:19:53 +0600

    updated

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,50 +1,57 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
-import { Post } from "../../../generated/prisma/client";
-
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
-
   try {
-    const user = req.user as any;
-    /* const {user} = req; */
-
+    const user = req.user;
     if (!user) {
       return res.status(400).json({
-        message: "Unauthorized access"
+        error: "Unauthorized!",
       })
-
     }
-    /*  console.log(req.user) */
-    const result = await postService.createPost(req.body, user.id as string);
+    const result = await postService.createPost(req.body, user.id as string)
     res.status(201).json(result)
   } catch (e) {
     res.status(400).json({
-      message: "Failed to post creation",
+      error: "Post creation failed",
       details: e
     })
   }
 }
+
 
 const getAllPost = async (req: Request, res: Response) => {
   try {
     const { search } = req.query
-
-    console.log("search value", search)
     const searchString = typeof search === 'string' ? search : undefined
+
     const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
-    const isFeatured = req.query.isFeatured ? req.query.isFeatured === 'true' : false;
-    console.log({ isFeatured });
-    const result = await postService.getAllPost({ search: searchString, tags, isFeatured });
+
+
+    // true or false
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === 'true'
+        ? true
+        : req.query.isFeatured === 'false'
+          ? false
+          : undefined
+      : undefined
+
+    const status = req.query.status as PostStatus | undefined
+
+    const authorId = req.query.authorId as string | undefined
+
+    const result = await postService.getAllPost({ search: searchString, tags, isFeatured, status, authorId })
     res.status(200).json(result)
-  }
-  catch (e) {
+  } catch (e) {
     res.status(400).json({
-      message: "Failed to get all posts",
+      error: "Post creation failed",
       details: e
     })
   }
 }
+
 export const PostController = {
   createPost,
   getAllPost
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,10 +1,8 @@
-
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
 import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (data: Omit<Post, 'id' | 'createdAt' | 'updatedAt' | 'authorId'>, userId: string) => {
-
     const result = await prisma.post.create({
         data: {
             ...data,
@@ -15,14 +13,20 @@ const createPost = async (data: Omit<Post, 'id' | 'createdAt' | 'updatedAt' | 'a
 }
 
 const getAllPost = async ({
-    search, tags, isFeatured
-
+    search,
+    tags,
+    isFeatured,
+    status,
+    authorId
 }: {
-    search: string | undefined
-    , tags: string[] | [],
-    isFeatured: boolean
+    search: string | undefined,
+    tags: string[] | [],
+    isFeatured: boolean | undefined,
+    status: PostStatus | undefined,
+    authorId: string | undefined
 }) => {
     const andConditions: PostWhereInput[] = []
+
     if (search) {
         andConditions.push({
             OR: [
@@ -46,6 +50,7 @@ const getAllPost = async ({
             ]
         })
     }
+
     if (tags.length > 0) {
         andConditions.push({
             tags: {
@@ -58,17 +63,29 @@ const getAllPost = async ({
         andConditions.push({
             isFeatured
         })
-    } {
-        const allPost = await prisma.post.findMany({
-            where: {
-                AND: andConditions
-            }
-        });
-        return allPost;
+    }
 
+    if (status) {
+        andConditions.push({
+            status
+        })
     }
+
+    if (authorId) {
+        andConditions.push({
+            authorId
+        })
+    }
+
+    const allPost = await prisma.post.findMany({
+        where: {
+            AND: andConditions
+        }
+    });
+    return allPost;
 }
-    export const postService = {
-        createPost,
-        getAllPost
-    }
+
+export const postService = {
+    createPost,
+    getAllPost
+}
